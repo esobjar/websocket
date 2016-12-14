155699427dfc25227f0c01f6dbd6e218b9a24d2b
Jaroslaw Sobolewski <jaroslaw.sobolewski@gft.com>
2016-12-14 17:14:53 +0100

    first commit

---
+++ src/main/java/com/websockets/SpringWebsocketsApplication.java
@@ -0,0 +1,12 @@
+package com.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketsApplication.class, args);
+	}
+}
